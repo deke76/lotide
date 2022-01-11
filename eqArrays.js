2c6ea8a7cb186924cfa4d97f3fd5c3507dc7fd7b
Darcy <darcy.lauder@shaw.ca>
2022-01-11 14:55:34 -0700

    added eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray[i]; i++) {
+    if (firstArray[i] !== secondArray[i]) return false;
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1,2,3],[1,2,]), true);
