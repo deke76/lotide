12e77a21aa6293933b03bc211242a0cbb158c06f
Darcy <darcy.lauder@shaw.ca>
2022-01-11 16:55:30 -0700

    proper setup for without

--- without.js
+++ without.js
@@ -1,12 +1,12 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = function(firstArray, secondArray) {
   for (let i = 0; i < firstArray[i]; i++) {
     if (firstArray[i] !== secondArray[i]) return false;
   }
   return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  eqArrays(actual, expected) ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
 const without = function(firstArray, secondArray) {
