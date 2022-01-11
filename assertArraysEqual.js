a804a66e27d77de6a0bc127e20fd9a1762c116c8
Darcy <darcy.lauder@shaw.ca>
2022-01-11 16:45:47 -0700

    assertArraysEqual visually verified

---
+++ assertArraysEqual.js
@@ -0,0 +1,14 @@
+const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray[i]; i++) {
+    if (firstArray[i] !== secondArray[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  eqArrays(actual, expected) ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertArraysEqual([1], [2]);
+assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual(['hello', 'world'], ['hello','world']);
