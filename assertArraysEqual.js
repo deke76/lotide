63e654dbc6bccfbc7ee5494b534088c73b78ba37
Darcy <darcy.lauder@shaw.ca>
2022-01-11 16:40:37 -0700

    assertArraysEqual.js added

--- assertArraysEqual.js
+++
@@ -1,6 +0,0 @@
-const eqArrays = function(firstArray, secondArray) {
-  for (let i = 0; i < firstArray[i]; i++) {
-    if (firstArray[i] !== secondArray[i]) return false;
-  }
-  return true;
-};
--- without.js
+++ without.js
@@ -12,6 +12,14 @@ const eqArrays = function(firstArray, secondArray) {
 const without = function(firstArray, secondArray) {
   let outputArray = [];
   for (let i = 0; i < firstArray.length; i++) {
-    (secondArray.includes(firstArray[i])) ? i++ : outputArray[i] = firstArray[i];
+    (secondArray.includes(firstArray[i])) ? i++ : outputArray.push(firstArray[i]);
   }
+  return outputArray;
 };
+
+//Test Cases
+console.log(without([1, 2, 3], [1]));
+console.log(without(['1','2','3'], [1,2,"3"]));
+
+const words = ['hello', 'world', 'lighthouse'];
+without(words, ['lighthouse']);
