03799ae3ad04e92370e1519a87bc9e62df94f73d
Darcy <darcy.lauder@shaw.ca>
2022-01-11 22:46:20 -0700

    formatting changes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,7 +6,9 @@ const eqArrays = function(firstArray, secondArray) {
 };
 
 const assertArraysEqual = function(actual, expected) {
-  eqArrays(actual, expected) ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  eqArrays(actual, expected)
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
 assertArraysEqual([1], [2]);
--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,7 @@
 const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
 const eqArrays = function(firstArray, secondArray) {
