3bbc1ff61e518f718ca2cd97553e41ddcda75a95
Darcy <darcy.lauder@shaw.ca>
2022-01-11 21:50:16 -0700

    adjusted comparison length

--- eqArrays.js
+++ eqArrays.js
@@ -3,11 +3,11 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(firstArray, secondArray) {
-  for (let i = 0; i < firstArray[i]; i++) {
+  for (let i = 0; i < Math.max(firstArray.length, secondArray.length); i++) {
     if (firstArray[i] !== secondArray[i]) return false;
   }
   return true;
 };
 
 //Test code
-assertEqual(eqArrays([1,2,3], [1,2,3]), true);
+assertEqual(eqArrays([1,2], [1,2,3]), true);
