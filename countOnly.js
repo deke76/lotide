62cf155cc730490658bc78200b6135b55e80054e
Darcy <darcy.lauder@shaw.ca>
2022-01-12 15:06:02 -0700

    first working version countOnly

--- countOnly.js
+++ countOnly.js
@@ -3,3 +3,35 @@ const assertEqual = function(actual, expected) {
     ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
     : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
+
+const countOnly = function(allItems, itemsToCount) {
+  let countedItems = {};
+  for (const key in itemsToCount) {
+    if (itemsToCount[key]) countedItems[key] = 0;
+  }
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) countedItems[item]++;
+  }
+  
+  return countedItems;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
