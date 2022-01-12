29f3d0144622c7c62aef7fe40180142f1adc4824
Darcy <darcy.lauder@shaw.ca>
2022-01-12 14:26:27 -0700

    formatting changes and added count only

--- countOnly.js
+++ countOnly.js
@@ -3,7 +3,3 @@ const assertEqual = function(actual, expected) {
     ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
     : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
-
-//TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
