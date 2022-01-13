5628177daac7f810e1f5f04febf55adc9a7176f4
Darcy <darcy.lauder@shaw.ca>
2022-01-12 17:59:23 -0700

    working countLetters and added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const letterPositions = function(stringToCount) {
+  let countedString = {};
+  for (const index in stringToCount) {
+    if (stringToCount[index] !== ' ') {
+      if (!countedString.hasOwnProperty(stringToCount[index])) {
+        countedString[stringToCount[index]] = [];
+      }
+      countedString[stringToCount[index]].push(index);
+    }
+  }
+  return countedString;
+};
+
+console.log(letterPositions("hello"));
