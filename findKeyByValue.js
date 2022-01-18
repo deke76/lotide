1873740bd218a95c835d2d25349576b12e5a41d0
Darcy <darcy.lauder@shaw.ca>
2022-01-17 18:40:50 -0700

    commenting in findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  actual === expected
-    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const findKeyByValue = function(objectToSearch, stringValue) {
   for (const key in objectToSearch) {
     if (objectToSearch[key] === stringValue) return key;
@@ -11,11 +5,11 @@ const findKeyByValue = function(objectToSearch, stringValue) {
   return undefined;
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
 
 // assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 // assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
